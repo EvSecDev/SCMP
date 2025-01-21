c79ae25aada22f2b6d473b3b549f1383f0dca299
EvSecDev <dev@evsec.net>
2025-01-20 19:08:26 -0800

    Changed Bumped version for last commit

--- controller_src/main.go
+++ controller_src/main.go
@@ -135,7 +135,7 @@ var FailTrackerMutex sync.Mutex
 
 // Program Meta Info
 const progCLIHeader string = "==== Secure Configuration Management Program ===="
-const progVersion string = "v3.4.0"
+const progVersion string = "v3.4.1"
 const usage = `Secure Configuration Management Program (SCMP)
   Deploy configuration files from a git repository to Linux servers via SSH
   Deploy ad-hoc commands and scripts to Linux servers via SSH
